be8f5d6e60f1ee38dba2b21ca1a6e2cf66652e97
Gordon <chengordon8@gmail.com>
2024-07-23 14:27:35 +0800

    Moved outside of subfolder

--- extraction_chain.py
+++ extraction_chain.py
@@ -2,8 +2,8 @@
 from langchain.chains import TransformChain
 from langchain.output_parsers import OutputFixingParser, PydanticOutputParser
 from langchain.prompts import PromptTemplate
-from extraction_pipeline.gpt_api import chat_completion, vision_completion
-from extraction_pipeline.prompt_template import fix_prompt_template
+from gpt_api import chat_completion, vision_completion
+from prompt_template import fix_prompt_template
 
 def fix_chain_fun(inputs):
         fix_prompt = PromptTemplate.from_template(fix_prompt_template)
