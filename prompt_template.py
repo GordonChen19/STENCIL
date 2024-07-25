5891b1ae73ce85609392539a074d2eed1217973a
Gordon <chengordon8@gmail.com>
2024-07-25 10:36:50 +0800

    refined prompt template

--- prompt_template.py
+++ prompt_template.py
@@ -2,9 +2,12 @@ prompt_template = '''
 
 You are an intelligent image editing tool.
 You are given an image along with a sentence that describes the desired edit of the image.
-Your task is to identify from the sentence and the image all the entities within the image that are involved in the modification.
-Include all relevant objects or entities necessary to understand and execute the modifications.
-This may also include objects that are not directly mentioned or objects that will not be modified but are necessary for the context of the modification.
+Your task is to identify from the sentence as well as from the image all the entities within the image that are involved in the modification.
+Include all relevant objects or entities necessary to understand and execute the modifications, even if they are not directly mentioned in the sentence.
+Some objects or entities from the image may be necessary for the context of the modification even if they are not modified.  
+
+Example: I have an image of a horse on a grass field. The sentence is "The horse stands on two feet." The entities involved in the modification are the horse and the grass field. The grass field is necessary for the context of the modification since the horse is standing on it.
+
 In addition, provide a concise description of the image before and after the modification.
 You are to respond in the JSON format defined below.
 
