637f5c3d7c01fcbc08c239bf3deb5dc67e740b55
Gordon <chengordon8@gmail.com>
2024-07-25 12:30:23 +0800

    data models

--- data_models.py
+++ data_models.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel, Field
 from typing import List
 
 class Entity(BaseModel):
-    modified_objects: List[str] = Field(description = '''Names of each entity that are modified in the image''')
-    context_objects: List[str] = Field(description = '''Names of each entity that are necessary for the context of the modification''')
+    modified_objects: List[str] = Field(description = '''Description of each entity that is modified in the image''')
+    context_objects: List[str] = Field(description = '''Description  of each entity that is necessary for the context of the modification''')
     before: str = Field(description = '''Concise description of the image prior to it being edited''')
     after: str = Field(description = '''Concise description of the image after it has been edited''')
--- prompt_template.py
+++ prompt_template.py
@@ -5,6 +5,7 @@ You are given an image along with a sentence that describes the desired edit of 
 Your task is to identify from the sentence as well as from the image all the entities within the image that are involved in the modification.
 Include all relevant objects or entities necessary to understand and execute the modifications, even if they are not directly mentioned in the sentence.
 Some objects or entities from the image may be necessary for the context of the modification even if they are not modified.  
+Describe all identified objects.
 
 Example: I have an image of a horse on a grass field. The sentence is "The horse stands on two feet." The entity that is involved in the modification is the horse. and the grass field. The grass field is necessary for the context of the modification since the horse is standing on it.
 
