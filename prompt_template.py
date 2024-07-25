f71fac30bad8ee7f4542f6f890c7b8e1b0ff70f8
Gordon <chengordon8@gmail.com>
2024-07-25 17:54:45 +0800

    Refined prompt template

--- prompt_template.py
+++ prompt_template.py
@@ -1,13 +1,25 @@
 prompt_template = '''
 
-You are an intelligent image editing tool.
-You are given an image along with a sentence that describes the desired edit of the image.
-Your task is to identify from the sentence as well as from the image all the entities within the image that are involved in the modification.
-Include all relevant objects or entities necessary to understand and execute the modifications, even if they are not directly mentioned in the sentence.
-Some objects or entities from the image may be necessary for the context of the modification even if they are not modified.  
-Describe all identified objects.
+You are an intelligent image editing assistant.
+You are given an image along with an edit prompt that describes the desired modification made to the image.
+Your task is to identify from the sentence as well as from the image all the entities within the image that are relevant in this modification.
+An entity is relevant if it:
 
-Example: I have an image of a horse on a grass field. The sentence is "The horse stands on two feet." The entity that is involved in the modification is the horse. and the grass field. The grass field is necessary for the context of the modification since the horse is standing on it.
+1. is being modified
+2. is explicitly mentioned in the edit prompt
+3. is interacting with a modified entity. This could be if the modified entity is sitting on, jumping over, looking at or interacting with the entity in some kind of manner
+4. An entity appears in front of the modifed entity
+
+
+Example: 
+--------------
+Image: A horse on a grass field in front of a house in the background. 
+Edit Prompt: "The horse stands on two feet." 
+Context Objects: The grass field (Since the horse is standing on it)
+Modified Objects: The horse
+
+The house should not be mentioned as it does not satisfy any of the four requirements.
+--------------
 
 In addition, provide a concise description of the image before and after the modification.
 You are to respond in the JSON format defined below.
