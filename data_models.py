154d9a12e3a7d0829e978e81cd3055e756afe170
Gordon <chengordon8@gmail.com>
2024-07-28 10:49:43 +0800

    reduced data model

--- data_models.py
+++ data_models.py
@@ -2,6 +2,4 @@ from pydantic import BaseModel, Field
 from typing import List
 
 class Entity(BaseModel):
-    modified_objects: List[str] = Field(description = '''Description of each entity that is modified in the image''')
-    context_objects: List[str] = Field(description = '''Description  of each entity that is necessary for the context of the modification''')
-    description: str = Field(description = '''Rewrite of the prompt to include the relevant elements''')
+    modified_objects: List[str] = Field(description = '''Description of each element that is modified in the image''', examples = ["Dog", "Bench"])
--- prompt_template.py
+++ prompt_template.py
@@ -1,26 +1,8 @@
 prompt_template = '''
 
 You are an intelligent image editing assistant.
-You are given an image and a prompt that describes the desired modification made to the image.
-Your task is to extract from the prompt as well as from the image the elements relevant to the modification.
-In addition, you are to rewrite the prompt such that it also includes the relevant elements.
-An relevant element is either:
-
-1. directly modified by the prompt
-2. interacting with the element being modified PRIOR TO modification. The could be an element that the modified element IS CURRENTLY sitting on, jumping over, looking at, etc.
-3. interacting with the element being modified AFTER modification. This could be an element that the modified element WILL BE sitting on, jumping over, looking at, etc.
-4. explicitly mentioned in the prompt
-5. overlaps with or layers over the entity being modified in the image.
-
-Example: 
---------------
-Image: A horse on a grass field in front of a house in the background. 
-Edit Prompt: "The horse stands on two feet." 
-Context Objects: The grass field (Since the horse is standing on it)
-Modified Objects: The horse
-
-The house should not be mentioned as it does not satisfy any of the four requirements.
---------------
+You are given an image and a prompt that describes a desired modification made to the image.
+Your task is to extract from the prompt as well as from the image the elements that are modified.
 
 You are to respond in the JSON format defined below.
 
