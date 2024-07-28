e095454e6e907ab8d9c19329b82af1711c28cfd5
Gordon <chengordon8@gmail.com>
2024-07-28 12:11:23 +0800

    entity -> objects/people/elements

--- data_models.py
+++ data_models.py
@@ -2,4 +2,4 @@ from pydantic import BaseModel, Field
 from typing import List
 
 class Entity(BaseModel):
-    modified_objects: List[str] = Field(description = '''Description of each entity that is modified in the image''', examples = ["Dog", "Bench"])
+    modified_objects: List[str] = Field(description = '''Description of each objects/people/elements that is modified in the image''', examples = ["Dog", "Bench"])
